a0c7b42c5890ac44b7b04651a16cda6eea3e0e94
RamkumarMohan6383 <95962773+RamkumarMohan6383@users.noreply.github.com>
2022-11-03 10:31:06 +0530

    Add files via upload

---
+++ QRCode_Generator_withlibqrencode/generator/qrcode_generator.h
@@ -0,0 +1,32 @@
+#ifndef QRCODE_GENERATOR_H
+#define QRCODE_GENERATOR_H
+
+#include <QObject>
+#include <QPainter>
+#ifdef Q_OS_WINDOWS
+#include <QLibrary>
+#endif
+#include "qrencode.h"
+
+class QRCodeGenerator : public QObject
+{
+public:
+    explicit QRCodeGenerator(QObject *parent = nullptr);
+
+#ifdef Q_OS_WINDOWS
+    static void Initialize();
+    static void Deinitialize();
+#endif
+    static bool GeneratePixmapFromText(QString &text, QPixmap &pixmap,
+                                       int width, int height);
+signals:
+
+public slots:
+private:
+#ifdef Q_OS_WINDOWS
+    static QLibrary *m_qrencodelib;
+#endif
+};
+
+#endif // QRCODE_GENERATOR_H
+
